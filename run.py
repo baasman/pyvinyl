b1deda7290d04ccef8cea2ab0391a32852b19dda
Boudewijn Aasman <boudeyz@gmail.com>
2017-08-18 19:32:36 -0400

    Only run app when RUN_LOCAL on, and make config setting env variable

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
+import os
 from app import create_app
 
-app = create_app('development')
+app = create_app(os.environ.get('FLASK_CONFIG'))
 
-if __name__ == '__main__':
+if app.config.get('RUN_LOCAL'):
     app.run(port=8010)
